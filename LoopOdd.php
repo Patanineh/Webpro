d95ce0f7566b9401b344fb7168858dca3ee4fb48
Patanineh <patanineh@gmail.com>
2023-12-22 11:02:29 +0700

    1

---
+++ LoopOdd.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $num = 1;
+        while ( $num < 10) {
+            if ($num % 2 == 0);
+            else echo "$num is Odd Number <br>";
+            $num ++;
+            }
+    ?>
+</body>
+</html>
